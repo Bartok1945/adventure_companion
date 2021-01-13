22b883cbf71bab7cce19d8a08f9158d29c6a46cd
Amita <panwar.amita@gmail.com>
2021-01-12 21:21:28 -0500

    updated the dropdown modifier

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     //open the spell modal
     $('#addModel').modal();
     //an array for the ability score
-    var standardArray = [-5, -4, -3, -2, -1, 0, +1, +2, +3, +4, +5, +6, +7, +8, +9, +10, +11, +12, +13, +14, +15, +16, +17, +18, +19, +20];
+    var standardArray = ["-5","-4","-3","-2","-1","0","+1","+2","+3","+4","+5","+6","+7","+8","+9","+10"];
     //function for the classes
     fillClass();
     //function to fill the strength drop down
